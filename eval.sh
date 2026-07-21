4770295cb9a901ed2fa2399d18a77efbe909b1dc
Ste517 <66008197+Ste517@users.noreply.github.com>
2026-07-21 10:12:34 +0200

    Modificato script di correzione

--- eval.sh
+++ eval.sh
@@ -1,4 +1,32 @@
-for i in {0..12}; do
-    ./main.out < "TestSet/input$i.txt" | diff -q - "TestSet/output$i.txt" > /dev/null && echo "Test $i: OK" || echo "Test $i: FALLITO ❌"
-done
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 
+echo "Compilazione in corso..."
+
+if g++ ./main.cpp -std=c++11 -o ./main.out > ./compilazione.log 2>&1; then
+    echo -e "${GREEN}Compilazione eseguita con successo${NC}\n"
+
+    echo "---------------------------------"
+    echo -e "Esecuzione dei test:\n"
+
+    passed=0
+    total=0
+
+    for file in $(ls TestSet/input*.txt | sort -V); do
+        ((total++))
+        i=$(echo "$file" | grep -oE '[0-9]+')
+
+        if ./main.out < "$file" | diff -q - "TestSet/output$i.txt" > /dev/null; then
+            echo -e "Test $i: ${GREEN}OK${NC}"
+            ((passed++))
+        else
+            echo -e "Test $i: ${RED}FALLITO${NC}"
+        fi
+    done
+
+    echo -e "\n---------------------------------"
+    echo "Esito: $passed/$total test superati."
+else
+    echo -e "${RED}La compilazione non è andata a buon fine${NC}, vedi compilazione.log per maggiori dettagli."
+fi
